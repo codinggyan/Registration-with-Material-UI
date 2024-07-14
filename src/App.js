e758a9eb27a71d653abb89506c5949c79aa673b4
codinggyan <sumit@Sumits-MacBook-Pro.local>
2024-07-14 20:32:32 +0530

    add the material-ui library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <h1>coding gyan !!</h1>
+      <Typography variant="h1" component="h2">
+        coding gyan !!
+      </Typography>
     </div>
   );
 }
